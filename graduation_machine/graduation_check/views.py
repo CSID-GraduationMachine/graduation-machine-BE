15a4933fa3360041b38fe081241ce26ff2daf447
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-13 18:40:45 +0900

    feat: 졸업판정 View 구현

---
+++ graduation_machine/graduation_check/views.py
@@ -0,0 +1,15 @@
+from services.graduation_check_service import GraduationCheckService
+from rest_framework import views, response
+from django.http import JsonResponse
+
+class GraduationCheckAPIView(views.APIView):
+
+    def post(self, request, *args, **kwargs):
+        year = self.request.query_params.get('year')
+        tech = self.request.query_params.get('tech')
+        excel_file = request.FILES.get('file')
+
+        if not excel_file.name.endswith('.xlsx'):
+            return JsonResponse({'error': 'File is not xlsx format'}, status=400)
+        
+        return response.Response(GraduationCheckService().check_graduation(year, tech, excel_file))
