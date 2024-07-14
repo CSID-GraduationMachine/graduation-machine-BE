8073cf6974345eb4087fd47d18c699996cc302df
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-14 23:14:02 +0900

    fix: dictionary 참조 문법 오류 수정 / 학점 평점 체크 로직 추가

--- graduation_machine/graduation_check/services/graduation_check_service.py
+++ graduation_machine/graduation_check/services/graduation_check_service.py
@@ -1,5 +1,5 @@
 from .utils.graduation_check_util import GraduationCheckUtil
-from ..models import LectureGroup, GraduationRequirements, GraduationRequirementsDetail, Lecture, LectureLectureGroup, Prerequest, CommonLectureGroup
+from ..models import LectureGroup, GraduationRequirements, GraduationRequirementsDetail, Lecture, LectureLectureGroup, Prerequest
 from rest_framework.response import Response
 
 class GraduationCheckService:
@@ -9,13 +9,16 @@ class GraduationCheckService:
         
         graduation_requirements = GraduationRequirements.objects.get(year = year, tech = tech)
 
-        user_lectures = GraduationCheckUtil.read_report_card(excel_file) # year, season, code, credit
+        user_lectures = GraduationCheckUtil.read_report_card(excel_file) # year, season, code, credit, grade
+        user_lecture_codes = [user_lecture['code'] for user_lecture in user_lectures]
 
         # 1. 총 이수 최소 학점 확인
         total_credit = 0 
         
         for user_lecture in user_lectures:
-            total_credit += user_lecture.credit
+            if(user_lecture['grade'] == 'F'):
+                continue
+            total_credit += user_lecture['credit']
         graduation_minimum_total_credit = graduation_requirements.total_minimum_credit
         if total_credit < graduation_minimum_total_credit:
             data['total_credit_check_message'] = f"{total_credit}/{graduation_minimum_total_credit}"
@@ -30,17 +33,17 @@ class GraduationCheckService:
             mandatory_lecture_group_list = LectureGroup.objects.filter(grd = graduation_requirements_detail, is_mandatory = True)
             result_list = [{lecture_group.lecture_group_name: False} for lecture_group in mandatory_lecture_group_list] # 각각의 필수 강의 그룹에 대해 False로 초기화된 리스트 생성
             for mandatory_lecture_group in mandatory_lecture_group_list: # 속해있는 필수 강의 그룹에 대해
-                mandatory_lecture_group_lectures = mandatory_lecture_group_lectures = LectureLectureGroup.objects.filter(lecture_group=mandatory_lecture_group).select_related('lecture') # 해당 필수 강의 그룹에 실제 맵핑되는 강의들을 mandatory_lecture_group_lectures에 저장
+                mandatory_lecture_group_lectures = LectureLectureGroup.objects.filter(lecture_group=mandatory_lecture_group).select_related('lecture') # 해당 필수 강의 그룹에 실제 맵핑되는 강의들을 mandatory_lecture_group_lectures에 저장
                 for mandatory_lecture_group_lecture in mandatory_lecture_group_lectures: # 각각의 강의 하나하나마다 
                     mandatory_lecture = Lecture.objects.get(id = mandatory_lecture_group_lecture.lecture.id) # 사용자의 수강 강의 목록에 해당 강의가 있는지 확인
-                    if mandatory_lecture in user_lectures: # 있다면
+                    if mandatory_lecture.code in user_lecture_codes: # 있다면
                         result_list[mandatory_lecture_group_list.index(mandatory_lecture_group)][mandatory_lecture_group.lecture_group_name] = True # 해당 필수 강의 그룹에 대해 True로 변경
                         break # 다음 필수 강의 그룹으로 넘어감
         data['mandatory_lecture_group_list'] = result_list
 
         # 3. 선수과목 수강 여부 확인
         for user_lecture in user_lectures:
-            lecture = Lecture.objects.get(year = user_lecture.year, season = user_lecture.season, code = user_lecture.code)
+            lecture = Lecture.objects.get(year = user_lecture['year'], season = user_lecture['season'], code = user_lecture['code'], credit = user_lecture['credit'])
             lecture_lecture_group = LectureLectureGroup.objects.get(lecture = lecture)
             lecture_group = LectureGroup.objects.get(id = lecture_lecture_group.lecture_group.id)
             prerequests = Prerequest.objects.filter(lecture_group = lecture_group)
@@ -59,18 +62,48 @@ class GraduationCheckService:
         for graduation_requirements_detail in graduation_requirements_details:
             satisfying_credit = 0
             for user_lecture in user_lectures:
-                lecture = Lecture.objects.get(year = user_lecture.year, season = user_lecture.season, code = user_lecture.code)
+                lecture = Lecture.objects.get(year = user_lecture['year'], season = user_lecture['season'], code = user_lecture['code'], credit = user_lecture['credit'])
                 lecture_lecture_group = LectureLectureGroup.objects.get(lecture = lecture)
                 lecture_group = LectureGroup.objects.get(id = lecture_lecture_group.lecture_group.id)
                 if lecture_group.id == graduation_requirements_detail.id:
-                    satisfying_credit += user_lecture.credit
+                    satisfying_credit += user_lecture['credit']
             if satisfying_credit < graduation_requirements_detail.minimum_credit:
                 data[f"{graduation_requirements_detail.requirements_name}_message"] = f"{satisfying_credit}/{graduation_requirements_detail.minimum_credit}"
                 data[f"{graduation_requirements_detail.requirements_name}_value"] = False
             else:
                 data[f"{graduation_requirements_detail.requirements_name}_message"] = f"{satisfying_credit}/{graduation_requirements_detail.minimum_credit}"
                 data[f"{graduation_requirements_detail.requirements_name}_value"] = True
-        
+
+        # 5. 학점 평점 2.0 이상 확인
+        user_lecture_score = 0.0
+
+        for user_lecture in user_lectures:
+            if user_lecture['grade'] == 'A+':
+                user_lecture_score += 4.5 * user_lecture['credit']
+            elif user_lecture['grade'] == 'A0':
+                user_lecture_score += 4.0 * user_lecture['credit']
+            elif user_lecture['grade'] == 'B+':
+                user_lecture_score += 3.5 * user_lecture['credit']
+            elif user_lecture['grade'] == 'B0':
+                user_lecture_score += 3.0 * user_lecture['credit']
+            elif user_lecture['grade'] == 'C+':
+                user_lecture_score += 2.5 * user_lecture['credit']
+            elif user_lecture['grade'] == 'C0':
+                user_lecture_score += 2.0 * user_lecture['credit']
+            elif user_lecture['grade'] == 'D+':
+                user_lecture_score += 1.5 * user_lecture['credit']
+            elif user_lecture['grade'] == 'D0':
+                user_lecture_score += 1.0 * user_lecture['credit']
+            elif user_lecture['grade'] == 'F':
+                user_lecture_score += 0.0 * user_lecture['credit']
+            else:
+                continue
+        if user_lecture_score / total_credit < 2.0:
+            data['grade_point_check_message'] = f"{user_lecture_score / total_credit}"
+            data['grade_point_check_value'] = False
+        else:
+            data['grade_point_check_message'] = f"{user_lecture_score / total_credit}"
+            data['grade_point_check_value'] = True
         # 결과 JSON 반환
         return Response(data)
 
