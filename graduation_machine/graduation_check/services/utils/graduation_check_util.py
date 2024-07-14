1c986cbd2fc3d6a4e351f0d448d82dd957e4b563
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-15 01:21:26 +0900

    fix: 성적표의 컬럼 형식을 Lecture 테이블과 통일

--- graduation_machine/graduation_check/services/utils/graduation_check_util.py
+++ graduation_machine/graduation_check/services/utils/graduation_check_util.py
@@ -1,15 +1,28 @@
 import pandas as pd
 
 class GraduationCheckUtil:
-    def read_report_card(self, excel_file):
+    def read_report_card(excel_file):
         df = pd.read_excel(excel_file, engine='openpyxl')
         
         user_lectures = []
         for index, row in df.iterrows():
+            lecture_code = f"{row['학수강좌번호']}-{str(row[df.columns[6]]).zfill(2)}"
+            season = ''
+            if row['학기'] == '1학기':
+                season = '1'
+            elif row['학기'] == '2학기':
+                season = '2'
+            elif row['학기'] == '여름학기':
+                season = 'summer'
+            elif row['학기'] == '겨울학기':
+                season = 'winter'
+            else:
+                season = row['학기']
+
             lecture_data = {
                 'year': row['년도'],
-                'season': row['학기'],
-                'code': row['학수강좌번호'],
+                'season': season,
+                'code': lecture_code,
                 'credit': row['학점'],
                 'grade': row['등급']
             }
