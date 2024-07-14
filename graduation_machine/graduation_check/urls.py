bef4bd91033758c9d17a379e7c2237de98146a88
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-07-14 23:07:32 +0900

    Chore: 코드 중복 제거 #2

--- graduation_machine/graduation_check/urls.py
+++ graduation_machine/graduation_check/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from .views import GraduationRequirementsViewSet
-from .views import LectureGroupViewSet
-from .views import LectureViewSet
-from .views import PrerequestViewSet
-from .views import CommonLectureGroupViewSet
+from .views import GraduationRequirementsViewSet, LectureGroupViewSet, LectureViewSet, PrerequestViewSet, CommonLectureGroupViewSet
 
 urlpatterns = [
+    # 학과 년도별 졸업이수 조건 조회
     path('graduation_conditions', GraduationRequirementsViewSet.as_view({'get': 'list'})),
+    # 해당 조건 공통강의 그룹 조회
     path('common_lecture_groups', LectureGroupViewSet.as_view({'get': 'list'})),
+    # 공통강의 개설 강의 내역 조회
     path('common_lecture_descriptions', LectureViewSet.as_view({'get': 'list'})),
+    # 선이수 목록 조회, 추가
     path('prerequests', PrerequestViewSet.as_view({'get': 'list', 'post': 'create'})),
+    # 전체 공통 강의 조회
     path('all_common_lectures', CommonLectureGroupViewSet.as_view({'get': 'list'})),
-    path('create_common_lecture_group', CommonLectureGroupViewSet.as_view({'post': 'create'})),
-    path('delete_common_lecture_group', CommonLectureGroupViewSet.as_view({'delete': 'destroy'})),
+    # 공통 강의 그룹 추가, 삭제
+    path('common_lecture_group', CommonLectureGroupViewSet.as_view({'post': 'create', 'delete': 'destroy'})),
 ]
--- graduation_machine/graduation_check/views.py
+++ graduation_machine/graduation_check/views.py
@@ -1,18 +1,20 @@
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import GraduationRequirementsDetailSerializer
+from .serializers import (
+    GraduationRequirementsDetailSerializer,
+    LectureGroupSerializer,
+    LectureSerializer,
+    PrerequestSerializer,
+    CommonLectureGroupSerializer,
+)
 from .services.graduation_requirements_service import GraduationRequirementService
-from .models import GraduationRequirements
-from .serializers import LectureGroupSerializer
 from .services.lecture_group_service import LectureGroupService
-from .serializers import LectureSerializer
 from .services.lecture_service import LectureService
 from .services.prerequest_service import PrerequestService
-from .serializers import PrerequestSerializer
-from .services.prerequest_service import PrerequestService
-from .serializers import CommonLectureGroupSerializer
 from .services.common_lecture_group_service import CommonLectureGroupService
+from .models import GraduationRequirements
+
 
 class GraduationRequirementsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = GraduationRequirementsDetailSerializer
@@ -32,6 +34,7 @@ class GraduationRequirementsViewSet(viewsets.GenericViewSet, mixins.ListModelMix
         except GraduationRequirements.DoesNotExist:
             return Response({"success": False, "error": "Graduation requirements not found"})
 
+
 class LectureGroupViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = LectureGroupSerializer
     permission_classes = [IsAuthenticated]
@@ -50,18 +53,14 @@ class LectureViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         group_id = request.query_params.get('id')
         lectures = LectureService.get_common_lecture_descriptions(group_id)
         prelectures = PrerequestService.get_prerequests()
-        prelecture_data = [{"pre_lecture_group_name": pre.prerequest_lecture_group.lecture_group_name, "pre_lecture_group_id": pre.prerequest_lecture_group.id} for pre in prelectures if pre.lecture_group.id == group_id]
+        prelecture_data = [
+            {"pre_lecture_group_name": pre.prerequest_lecture_group.lecture_group_name,
+             "pre_lecture_group_id": pre.prerequest_lecture_group.id}
+            for pre in prelectures if pre.lecture_group.id == group_id
+        ]
         lecture_data = LectureSerializer(lectures, many=True).data
         return Response({"success": True, "data": [prelecture_data, lecture_data], "error": None})
-    
 
-class PrerequestViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    serializer_class = PrerequestSerializer
-    permission_classes = [IsAuthenticated]
-
-    def list(self, request, *args, **kwargs):
-        prerequests = PrerequestService.get_prerequests()
-        return Response({"success": True, "data": PrerequestSerializer(prerequests, many=True).data, "error": None})
 
 class PrerequestViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = PrerequestSerializer
@@ -76,22 +75,22 @@ class PrerequestViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.C
         prerequest_lecture_group_id = request.data.get('prerequest_lecture_group_id')
         PrerequestService.add_prerequest(lecture_group_id, prerequest_lecture_group_id)
         return Response({"success": True, "data": None, "error": None})
-    
 
-class CommonLectureGroupViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+
+class CommonLectureGroupViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin):
     serializer_class = CommonLectureGroupSerializer
     permission_classes = [IsAuthenticated]
 
     def list(self, request, *args, **kwargs):
         common_lectures = CommonLectureGroupService.get_all_common_lectures()
         return Response({"success": True, "data": CommonLectureGroupSerializer(common_lectures, many=True).data, "error": None})
-    
+
     def create(self, request, *args, **kwargs):
         lecture_ids = request.data.get('lecture_id')
         common_group_name = request.data.get('lecture_group_name')
         CommonLectureGroupService.create_common_lecture_group(lecture_ids, common_group_name)
         return Response({"success": True, "data": None, "error": None})
-    
+
     def destroy(self, request, *args, **kwargs):
         common_group_name = request.data.get('lecture_group_name')
         CommonLectureGroupService.delete_common_lecture_group(common_group_name)
