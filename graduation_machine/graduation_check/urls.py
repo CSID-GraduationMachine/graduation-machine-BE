f3b4c95928b6d1610e29ca69cf5d53e049c0bf6f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-15 05:22:28 +0900

    fix: 언어스코어 -> 하이픈 변경 / 특정 공통 강의 그룹에 속한 강의들 조회 api 라우팅

--- graduation_machine/graduation_check/urls.py
+++ graduation_machine/graduation_check/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from .views import GraduationRequirementsViewSet, LectureGroupViewSet, LectureViewSet, PrerequestViewSet, CommonLectureGroupViewSet, GraduationCheckAPIView
+from .views import GraduationRequirementsViewSet, LectureGroupViewSet, LectureViewSet, PrerequestViewSet, CommonLectureGroupViewSet, GraduationCheckAPIView, LecturesInCommonGroupAPIView
 
 router = SimpleRouter()
 
 urlpatterns = [
     # 학과 년도별 졸업이수 조건 조회
-    path('graduation_conditions', GraduationRequirementsViewSet.as_view({'get': 'list'})),
+    path('graduation-conditions', GraduationRequirementsViewSet.as_view({'get': 'list'})),
     # 해당 조건 공통강의 그룹 조회
-    path('common_lecture_groups', LectureGroupViewSet.as_view({'get': 'list'})),
+    path('common-lecture-groups', LectureGroupViewSet.as_view({'get': 'list'})),
     # 공통강의 개설 강의 내역 조회
-    path('common_lecture_descriptions', LectureViewSet.as_view({'get': 'list'})),
+    path('common-lecture-descriptions', LectureViewSet.as_view({'get': 'list'})),
     # 선이수 목록 조회, 추가
     path('prerequests', PrerequestViewSet.as_view({'get': 'list', 'post': 'create'})),
     # 공통 강의 그룹 조회, 추가, 삭제
-    path('common_lecture_group', CommonLectureGroupViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'})),
+    path('common-lecture-group', CommonLectureGroupViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'})),
+    # 특정 공통 강의 그룹에 속한 강의들 조회
+    path('common-lecture-group/lectures', LecturesInCommonGroupAPIView.as_view(), name='common-lecture-group-lectures'),
     # 졸업 요건 만족 검사
     path('check', GraduationCheckAPIView.as_view(), name='graduation-check'),
 ]
--- graduation_machine/graduation_machine/urls.py
+++ graduation_machine/graduation_machine/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/graduation_check/', include('graduation_check.urls')),
+    path('api/v1/graduation-check/', include('graduation_check.urls')),
 ]
