4e1bb4b4cafba7571d861ed1fc68965046aa26e1
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-17 08:37:51 +0900

    feat: 졸업요건 서비스 구현

--- graduation_machine/graduation_check/services/graduation_requirements_service.py
+++ graduation_machine/graduation_check/services/graduation_requirements_service.py
@@ -1,48 +1,48 @@
-from graduation_check.models import GraduationRequirements, GraduationRequirementsDetail, LectureGroup, LectureLectureGroup, Prerequest
+from graduation_check.models import GraduationRequirements
 from django.shortcuts import get_object_or_404
 
 class GraduationRequirementService:
     @staticmethod
-    def get_graduation_conditions(year, tech):
+    def get_total_minimum_credit(requirement_id):
         try:
-            return GraduationRequirements.objects.get(year=year, tech=tech)
+            return get_object_or_404(GraduationRequirements, pk=requirement_id).total_minimum_credit
         except GraduationRequirements.DoesNotExist:
+            return None
+    @staticmethod
+    def get_graduation_requirements():
+        try:
+            return GraduationRequirements.objects.all()
+        except Exception as e:
+            print(f"An unexpected error occurred while fetching graduation requirements: {str(e)}")
             return None
 
     @staticmethod
-    def get_graduation_requirements_details(requirement_id):
-        try:
-            graduation_requirement = get_object_or_404(GraduationRequirements, pk=requirement_id)
-            return GraduationRequirementsDetail.objects.filter(gr=graduation_requirement)
-        except GraduationRequirements.DoesNotExist:
-            return None
-        
+    def create_graduation_requirements(year, tech, total_minimum_credit):
+        return GraduationRequirements.objects.create(year=year, tech=tech, total_minimum_credit=total_minimum_credit)
+
     @staticmethod
-    def update_graduation_conditions(requirement_id, data):
+    def update_graduation_requirements(requirement_id, year, tech, total_minimum_credit):
         try:
-            requirement = GraduationRequirementsDetail.objects.get(id=requirement_id)
-            requirement.minimum_credit = data.get('minimum_credit')
-            requirement.requirements_name = data.get('requirement_name')
+            requirement = GraduationRequirements.objects.get(id=requirement_id)
+            requirement.year = year
+            requirement.tech = tech
+            requirement.total_minimum_credit = total_minimum_credit
             requirement.save()
             return requirement
         except GraduationRequirements.DoesNotExist:
             return None
         except Exception as e:
-            print(f"An unexpected error occurred while updating graduation conditions: {str(e)}")
+            print(f"An unexpected error occurred while updating graduation requirements: {str(e)}")
             return None
-        
+
     @staticmethod
-    def delete_graduation_conditions(requirement_id):
+    def delete_graduation_requirements(requirement_id):
         try:
-            requirement = GraduationRequirementsDetail.objects.get(id=requirement_id)
-            lecture_group = LectureGroup.objects.filter(grd=requirement)
-            LectureLectureGroup.objects.filter(lecture_group__in=lecture_group).delete()
-            Prerequest.objects.filter(lecture_group__in=lecture_group).delete()
-            lecture_group.delete()
+            requirement = GraduationRequirements.objects.get(id=requirement_id)
             requirement.delete()
             return True
         except GraduationRequirements.DoesNotExist:
             return False
         except Exception as e:
-            print(f"An unexpected error occurred while deleting graduation conditions: {str(e)}")
+            print(f"An unexpected error occurred while deleting graduation requirements: {str(e)}")
             return None
