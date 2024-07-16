97a9a7df581720aba89868274070a659efe1477e
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-17 08:38:43 +0900

    fix: 공통 url 수정

--- graduation_machine/graduation_machine/urls.py
+++ graduation_machine/graduation_machine/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/graduation-check/', include('graduation_check.urls')),
+    path('api/v1/', include('graduation_check.urls')),
 ]
