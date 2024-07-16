8ede9f32d5dab325cff13e6159c5440dc9a83cda
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-17 08:38:30 +0900

    feat: 선이수 서비스 구현

--- graduation_machine/graduation_check/services/prerequest_service.py
+++ graduation_machine/graduation_check/services/prerequest_service.py
@@ -2,30 +2,27 @@ from graduation_check.models import Prerequest, LectureGroup
 
 class PrerequestService:
     @staticmethod
-    def get_prerequests():
+    def get_prerequests(lecture_group_id):
         try:
-            return Prerequest.objects.all()
+            return Prerequest.objects.filter(lecture_group_id=lecture_group_id)
         except Exception as e:
             print(f"An unexpected error occurred while fetching prerequests: {str(e)}")
             return None
 
     @staticmethod
-    def add_prerequest(lecture_group_id, prerequest_lecture_group_id):
-        return Prerequest.objects.create(lecture_group_id=lecture_group_id, prerequest_lecture_group_id=prerequest_lecture_group_id)
-
-    @staticmethod
-    def add_prerequest(lecture_group_id, prerequest_lecture_group_id):
+    def create_prerequest(lecture_group_id, prerequest_lecture_group_id):
         try:
-            # Ensure both lecture groups exist before creating a prerequest
-            if not (LectureGroup.objects.filter(id=lecture_group_id).exists() and 
-                    LectureGroup.objects.filter(id=prerequest_lecture_group_id).exists()):
-                return {"status": "error", "message": "One or both lecture groups do not exist."}
-            # Create the prerequest if both lecture groups exist
-            Prerequest.objects.create(
-                lecture_group_id=lecture_group_id, 
-                prerequest_lecture_group_id=prerequest_lecture_group_id
-            )
-            return {"status": "success", "message": "Prerequest added successfully."}
+            return Prerequest.objects.create(lecture_group_id=lecture_group_id, prerequest_lecture_group_id=prerequest_lecture_group_id)
         except Exception as e:
-            # Handle any unexpected exceptions
-            return {"status": "error", "message": f"An unexpected error occurred: {str(e)}"}
+            print(f"An unexpected error occurred while creating prerequest: {str(e)}")
+            return None
+        
+    @staticmethod
+    def delete_prerequest(id):
+        try:
+            prerequest = Prerequest.objects.get(id=id)
+            prerequest.delete()
+            return True
+        except Exception as e:
+            print(f"An unexpected error occurred while deleting prerequest: {str(e)}")
+            return None
