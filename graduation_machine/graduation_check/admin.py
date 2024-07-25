a20b464afa19eee93cbbfbf53af4e9c2f53b85b4
장동겸 <jangelliot0404@dgu.ac.kr>
2024-07-25 21:46:38 +0900

    ✨ Feature/#14 장고 어드민 설정 / CORS 설정 (#15)
    * feat: django admin 셋팅
    
    * feat: CORS 설정

--- graduation_machine/graduation_check/admin.py
+++ graduation_machine/graduation_check/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CommonLectureGroup, CommonLectureGroupLectureIdentification, Condition, LectureCondition, LectureGroup, LectureIdentification, LectureIdentificationLectureGroup, Prerequest
 
 # Register your models here.
+admin.site.register(CommonLectureGroup)
+admin.site.register(CommonLectureGroupLectureIdentification)
+admin.site.register(Condition)
+admin.site.register(LectureCondition)
+admin.site.register(LectureGroup)
+admin.site.register(LectureIdentification)
+admin.site.register(LectureIdentificationLectureGroup)
+admin.site.register(Prerequest)
